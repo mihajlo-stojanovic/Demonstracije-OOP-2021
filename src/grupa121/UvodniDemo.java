8c3b038a598fbb0458a37a5237f8dc0d39827219
mstojanovic6119rn <mstojanovic6119rn@raf.rs>
2022-03-02 21:21:00 +0100

    Initial commit

---
+++ src/grupa121/UvodniDemo.java
@@ -0,0 +1,7 @@
+package grupa121;
+
+public class UvodniDemo {
+    public static void main(String[] args) {
+        System.out.println("Welcome, 121");
+    }
+}
---
+++ src/grupa122/UvodniDemo.java
@@ -0,0 +1,7 @@
+package grupa122;
+
+public class UvodniDemo {
+    public static void main(String[] args) {
+        System.out.println("Welcome, 122");
+    }
+}
